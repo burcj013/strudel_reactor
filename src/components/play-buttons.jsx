8311876dcddeea5e7356a029e59c7c79db911932
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-15 18:07:43 +1030

    Removed unused processing buttons

--- src/components/play-buttons.jsx
+++ src/components/play-buttons.jsx
@@ -6,8 +6,8 @@ function PlayButtons({ onPlay, onPause }) {
             <div className="btn-group btn-group-lg gap-1 pt-3">
                 <button id="play" className="btn btn-secondary" title="Play" onClick={onPlay}><BsFillPlayFill /></button>
                 <button id="stop" className="btn btn-secondary" title="Pause" onClick={onPause}><BsFillPauseFill /></button>
-                <button id="process" className="btn btn-secondary">Preprocess</button>
-                <button id="process_play" className="btn btn-secondary">Proc & Play</button>
+                {/* <button id="process" className="btn btn-secondary">Preprocess</button>
+                <button id="process_play" className="btn btn-secondary">Proc & Play</button> */}
             </div>
         </>
     );
