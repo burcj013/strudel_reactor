a8d3a28471de9539a52bccd77f645ab563dbcafa
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-15 18:06:57 +1030

    Added volume slider and fixed some stuff

--- src/components/dj-controls.jsx
+++ src/components/dj-controls.jsx
@@ -1,15 +1,23 @@
 import InstrumentControls from "./instrument-controls";
 import './component-styling.css'
 
-function DJControls() {
+function DJControls( {volumeChange, onVolumeChange} ) {
     return (
         <>
+            <div className="input-group mb-3">
+                <span className="input-group-text bg-secondary border-secondary text-light" id="setCpmLabel">Set CPM</span>
+                <input type="text" className="form-control bg-light border-secondary" aria-describedby="setCpmLabel"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="volumeRange" className="form-label pt-3">Volume</label>
+                <input type="range" className="form-range slider" min="0" max="1" step="0.1" id="volumeRange" onChange={onVolumeChange}></input>
+            </div>
             <div className="form-check form-switch">
-                <label class="form-check-label px-2" for="instrumentSwitch">p1</label>
-                <input class="form-check-input form-switch bg-secondary border-dark" type="checkbox" role="switch" id="instrumentSwitch" />
+                <label className="form-check-label px-2" htmlFor="instrumentSwitch">p1</label>
+                <input className="form-check-input form-switch bg-secondary border-dark" type="checkbox" role="switch" id="instrumentSwitch" />
             </div>
 
-            <InstrumentControls/>
+            {/* <InstrumentControls/> */}
         </>
     );
 }
