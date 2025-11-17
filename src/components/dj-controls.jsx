73a01601589bd3d9ecfa61a1e556f936836c0063
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-17 21:32:52 +1030

    Fixed cpm input - is now functional

--- src/components/dj-controls.jsx
+++ src/components/dj-controls.jsx
@@ -4,9 +4,13 @@ function DJControls( {volumeChange, onVolumeChange, cpmChange, onCpmChange} ) {
     return (
         <>
             <div className="input-group mb-3 pt-5">
-                <span className="input-group-text bg-secondary border-secondary text-light" id="setCpmLabel" onChange={onCpmChange} type="number">Set CPM</span>
-                <input type="text" className="form-control bg-light border-secondary" aria-describedby="setCpmLabel"/>
+                <span className="input-group-text bg-secondary border-secondary text-light" id="setCpmLabel">Set CPM</span>
+                <input type="text" className="form-control bg-light border-secondary" aria-describedby="setCpmLabel" onChange={onCpmChange} value={cpmChange}/>
             </div>
+             <div className="mb-3">                
+                    <label htmlFor="volumeRange" className="form-label">Volume</label>
+                    <input type="range" className="form-range slider" min="0" max="1" step="0.1" id="volumeRange" onChange={onVolumeChange}></input>
+                </div>
 
             {/* <InstrumentControls/> */}
         </>
