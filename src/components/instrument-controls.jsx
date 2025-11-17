18b29fab37e176f38bc3ba1148285f40b961cf38
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-17 16:46:35 +1030

    Added code to get instruments and display their controls

--- src/components/instrument-controls.jsx
+++ src/components/instrument-controls.jsx
@@ -1,7 +1,32 @@
-function InstrumentControls() {
-    return (
-        <>
-        </>
+function InstrumentControls( {songText} ) {
+
+    let output = ``
+    let songLines = songText.split('\n')
+    let instruments = []
+
+    songLines.forEach(line => {
+        if (line.includes(": ")){
+            let instrument = line.match(/^.*: */)
+            instruments.push(instrument)
+        }
+    })
+
+    //onChange={onVolumeChange} put this back
+    return ( 
+        instruments.map((instrument) => (
+            <div key={instrument}>
+                <p className="text-light pt-3 border-top border-secondary">{instrument}</p>
+                <div className="mb-3">                
+                    <label htmlFor="volumeRange" className="form-label">Volume</label>
+                    <input type="range" className="form-range slider" min="0" max="1" step="0.1" id="volumeRange"></input>
+                </div>
+                <div className="form-check form-switch pb-3">
+                    <label className="form-check-label px-2" htmlFor="instrumentSwitch">On/Off</label>
+                    <input className="form-check-input form-switch bg-secondary border-dark" type="checkbox" role="switch" id="instrumentSwitch" />
+                </div>
+            </div>
+        )
+    )
     )
 }
 
