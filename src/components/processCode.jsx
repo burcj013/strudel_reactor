f12eda060889afcaff82c8626720715ae54f2021
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-17 16:48:13 +1030

    Redid function to process code line by line, started implementing various options

--- src/components/processCode.jsx
+++ src/components/processCode.jsx
@@ -1,4 +1,4 @@
-function ProcessCode( {songText, songVolume} ) {
+function ProcessCode( {songText, songVolume, cpmValue} ) {
 
     //let instruments = []
     //const songTextLines = songText.split('\n');
@@ -24,39 +24,76 @@ function ProcessCode( {songText, songVolume} ) {
     //     );
     // }
 
-    let outputText = songText
-    console.log("in proccode func" + songVolume)
-    
-    let regex = /[a-zA-Z0-9_]+:\s*\n[\s\S]+?\r?\n(?=[a-zA-Z0-9_]*[:\/])/gm;
+    let songLines = songText.split('\n')
+    let gainIndex = -1
+    let cpmIndex = -1
+    let i = 0
+    let outputText = ""
+    let instruments = []
 
-    let m;
-    let matches = []
-
-    while ((m = regex.exec(outputText)) !== null){
-
-        if (m.index === regex.lastIndex){
-            regex.lastIndex++
+    songLines.forEach(line => {
+        if (line.includes("all(x => x.gain(")){
+            gainIndex = i
         }
 
-        m.forEach((match, groupIndex) => {
-            matches.push(match)
-        })
+        if (line.includes("setcpm(")){
+            cpmIndex = i
+        }
+
+        if (line.includes(": ")){
+            let instrument = line.match(/^(.*?):/)
+            instruments.push(instrument)
+        }
+
+        i++;
+    })
+
+    if(gainIndex !== -1){
+        songLines[gainIndex] = `all(x => x.gain(${songVolume}))`
+    }
+    else { 
+        songLines.push(`all(x => x.gain(${songVolume})`)
+    }
+    
+    if(cpmIndex !== -1){
+        songLines[cpmIndex] = `setcpm(${cpmValue})`
+    }
+    else {
+        songLines.unshift(`setcpm(${cpmValue})`)
     }
 
-    let matches2 = matches.map(
-        match => match.replaceAll(/(?<!post)gain\(([\d.]+)\)/g, (match, captureGroup) =>
-            //need to fix this
-            `gain(${captureGroup * songVolume})`
-        )
-    )
-
-    let matches3 = matches.reduce(
-        (text, original, i) => text.replaceAll(original, matches2[i]),
-        outputText
-    )
-
-    console.log(matches3)
+    outputText = songLines.join('\n')
     
-    return matches3
+    // let regex = /[a-zA-Z0-9_]+:\s*\n[\s\S]+?\r?\n(?=[a-zA-Z0-9_]*[:\/])/gm;
+
+    // let m;
+    // let matches = []
+
+    // while ((m = regex.exec(outputText)) !== null){
+
+    //     if (m.index === regex.lastIndex){
+    //         regex.lastIndex++
+    //     }
+
+    //     m.forEach((match, groupIndex) => {
+    //         matches.push(match)
+    //     })
+    // }
+
+    // let matches2 = matches.map(
+    //     match => match.replaceAll(/(?<!post)gain\(([\d.]+)\)/g, (match, captureGroup) =>
+    //         //need to fix this
+    //         `gain(${captureGroup * songVolume})`
+    //     )
+    // )
+
+    // let matches3 = matches.reduce(
+    //     (text, original, i) => text.replaceAll(original, matches2[i]),
+    //     outputText
+    // )
+    
+    //need to do smth with the instruments, probs loop over them in instrument-controls to display mute switch and vol slider for each
+    return outputText
 }
+
 export default ProcessCode
