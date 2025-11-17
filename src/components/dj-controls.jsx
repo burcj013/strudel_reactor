6cfac5e11d74071719b28fb78125188cf3747706
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-17 16:46:01 +1030

    Moved slider and switch to instrument controls function

--- src/components/dj-controls.jsx
+++ src/components/dj-controls.jsx
@@ -1,20 +1,11 @@
-import InstrumentControls from "./instrument-controls";
 import './component-styling.css'
 
-function DJControls( {volumeChange, onVolumeChange} ) {
+function DJControls( {volumeChange, onVolumeChange, cpmChange, onCpmChange} ) {
     return (
         <>
-            <div className="input-group mb-3">
-                <span className="input-group-text bg-secondary border-secondary text-light" id="setCpmLabel">Set CPM</span>
+            <div className="input-group mb-3 pt-5">
+                <span className="input-group-text bg-secondary border-secondary text-light" id="setCpmLabel" onChange={onCpmChange} type="number">Set CPM</span>
                 <input type="text" className="form-control bg-light border-secondary" aria-describedby="setCpmLabel"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="volumeRange" className="form-label pt-3">Volume</label>
-                <input type="range" className="form-range slider" min="0" max="1" step="0.1" id="volumeRange" onChange={onVolumeChange}></input>
-            </div>
-            <div className="form-check form-switch">
-                <label className="form-check-label px-2" htmlFor="instrumentSwitch">p1</label>
-                <input className="form-check-input form-switch bg-secondary border-dark" type="checkbox" role="switch" id="instrumentSwitch" />
             </div>
 
             {/* <InstrumentControls/> */}
