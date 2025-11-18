b53f284faaa1a895ae6c8b82c3750fa61b96c2e0
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-18 21:19:06 +1030

    Changed the formatting a bit

--- src/components/play-buttons.jsx
+++ src/components/play-buttons.jsx
@@ -3,11 +3,9 @@ import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 function PlayButtons({ onPlay, onPause }) {
     return (
         <>
-            <div className="btn-group btn-group-lg gap-1 pt-3">
+            <div className="btn-group btn-group-lg gap-1 pt-3 w-100">
                 <button id="play" className="btn btn-secondary" title="Play" onClick={onPlay}><BsFillPlayFill /></button>
                 <button id="stop" className="btn btn-secondary" title="Pause" onClick={onPause}><BsFillPauseFill /></button>
-                {/* <button id="process" className="btn btn-secondary">Preprocess</button>
-                <button id="process_play" className="btn btn-secondary">Proc & Play</button> */}
             </div>
         </>
     );
