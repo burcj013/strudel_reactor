e2d7efb426d23df2acd115902aab8d4f96614559
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-18 21:18:57 +1030

    Fixed an issue with text not updating correctly

--- src/components/PreprocessText.jsx
+++ src/components/PreprocessText.jsx
@@ -1,9 +1,9 @@
 
-function PreprocessText({ defaultValue, onChange }) {
+function PreprocessText({ songText, onChange }) {
 
     return (
         <div>   
-            <textarea style={{ fontFamily: 'Lucida Console, monospace' }} className="form-control bg-dark text-light border-secondary rounded-top-0" rows="13" defaultValue={defaultValue} onChange={onChange} id="proc" ></textarea>
+            <textarea style={{ fontFamily: 'Lucida Console, monospace' }} className="form-control bg-dark text-light border-secondary rounded-top-0" rows="13" defaultValue={songText} value={songText} onChange={onChange} id="proc" ></textarea>
         </div>
     );
 }
