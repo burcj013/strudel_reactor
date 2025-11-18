bb3a21ec16042ebb5a554212571c9c5f461375b4
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-18 22:30:11 +1030

    Added comments

--- src/components/PreprocessText.jsx
+++ src/components/PreprocessText.jsx
@@ -1,6 +1,6 @@
 
 function PreprocessText({ songText, onChange }) {
-
+    // Used to display text editor
     return (
         <div>   
             <textarea style={{ fontFamily: 'Lucida Console, monospace' }} className="form-control bg-dark text-light border-secondary rounded-top-0" rows="13" defaultValue={songText} value={songText} onChange={onChange} id="proc" ></textarea>
